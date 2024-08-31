2f0dd12312e5d4f4e1476b80d01dfc905169cf83
Ibrahim Diallo <ibuvdiallo@gmail.com>
2024-08-31 14:57:57 -0700

    add toc to readme

--- README.md
+++ README.md
@@ -2,6 +2,31 @@
 
 This repository serves to track the progress of the book [Automated Agents](https://www.automatedagentsbook.com/). It allows the public to point errors and offer corrections along the way.
 
+Current Status: **Draft**
+
+
+
+## Table of Content
+
+
+* **Part One:** Understanding Chatbots
+	* **Chapter 1:** A Place to Start
+	* **Chapter 2:** Solving People Problems
+	* **Chapter 3:** Integration
+	* **Chapter 4:** Artificial Intelligence
+	* **Chapter 5:** Protecting Users
+* **Part Two:** Under the Hood
+	* **Chapter 6:** Integrating AI 
+	* **Chapter 7:** Integrating a Help Desk
+	* **Chapter 8:** Integrating an eCommerce Platform
+	* **Chapter 9:** Integrating a Carrier
+	* **Chapter 10:** Designing a Company Policy Processor
+* **Part Three**
+	* TBD
+
+---
+
+
 # TODO:
 
 - [x] Import the book in it's current state to github
--- src/test.js
+++ src/test.js
@@ -1,6 +1,7 @@
 import { HighlightJS as hljs} from "highlight.js";
 import * as markdown from "./lib/utils/markdown.js";
 
+
 import * as commonmark  from "commonmark";
 
 const reader = new commonmark.Parser();
@@ -26,34 +27,6 @@ if (name === "blue") {
 ${ticks}
 `;
 
-/*
-
-function highlightCode(htmlStr) {
-	const root = parse(htmlStr, { blockTextElements: { code: true }});
-	const elements = root.querySelectorAll("code");
-	elements.map(e => {
-		let lang = null;
-		let code = "";
-		if (e.attributes["class"]) {
-			lang = e.attributes["class"].split("-")[1];
-		}
-		if (lang) {
-			code = hljs.highlight(e.text, { language: lang}).value;
-		} else {
-			code = hljs.highlightAuto(e.text);
-		}
-		e.set_content(code);
-	});
-	return root.toString();
-}
-
-const data = markdown.parse(text);
-
-const code = highlightCode(data);
-console.log(code);
-
-*/
-
 function hlCode(parsed) {
 	const walker = parsed.walker();
 	let event;
@@ -74,22 +47,3 @@ const parsed = reader.parse(text);
 
 const code = hlCode(parsed);
 console.log(code)
-
-/*
-const a = reader.parse(text);
-
-
-const walker = a.walker();
-let event = null;
-while ((event = walker.next())) {
-    let type = event.node.type;
-    if (type === "code_block") {
-    	const code = event.node._literal
-    	const lang = event.node.info;
-    	const res = hljs.highlight(code, { language: lang});
-    	event.node._literal = res.value;
-    }
-}
-
-const output = writer.render(a);
-*/
