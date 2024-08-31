f6ee64334fb6375e2190fed7195e0f62c0f8be03
Ibrahim Diallo <ibuvdiallo@gmail.com>
2024-08-30 19:22:02 -0700

    add default syntax highlight colors

--- src/test.js
+++ src/test.js
@@ -1,11 +1,73 @@
-import { h, html } from "./lib/utils/html.js";
+import { HighlightJS as hljs} from "highlight.js";
+import * as markdown from "./lib/utils/markdown.js";
 
-const out = h("ul", { class: "test"}, [
-	h("li", { class: ["hello", "world"]}, "1"),
-	h("li", {}, "2"),
-	h("li", {}, "3"),
-]);
+import { parse } from "node-html-parser";
 
-const res = html(out);
+import * as commonmark  from "commonmark";
 
-console.log(res)
+const reader = new commonmark.Parser();
+const writer = new commonmark.HtmlRenderer();
+
+
+
+
+const ticks = "```";
+const text = `
+# Hello World
+
+Below is the code I need.
+
+${ticks}js
+const name = "Ibrahim Diallo";
+name.split("").map(b => {
+	console.log(b);
+});
+
+if (name === "blue") {
+	return 123;
+}
+${ticks}
+`;
+
+function highlightCode(htmlStr) {
+	const root = parse(htmlStr, { blockTextElements: { code: true }});
+	const elements = root.querySelectorAll("code");
+	elements.map(e => {
+		let lang = null;
+		let code = "";
+		if (e.attributes["class"]) {
+			lang = e.attributes["class"].split("-")[1];
+		}
+		if (lang) {
+			code = hljs.highlight(e.text, { language: lang}).value;
+		} else {
+			code = hljs.highlightAuto(e.text);
+		}
+		e.set_content(code);
+	});
+	return root.toString();
+}
+
+const data = markdown.parse(text);
+
+const code = highlightCode(data);
+console.log(code);
+
+/*
+const a = reader.parse(text);
+
+
+const walker = a.walker();
+let event = null;
+while ((event = walker.next())) {
+    let type = event.node.type;
+    if (type === "code_block") {
+    	const code = event.node._literal
+    	const lang = event.node.info;
+    	const res = hljs.highlight(code, { language: lang});
+    	event.node._literal = res.value;
+    }
+}
+
+const output = writer.render(a);
+*/
