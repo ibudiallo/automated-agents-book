51f77348c2a7699d89d42f815ed0cc07f9a464c4
Ibrahim Diallo <ibuvdiallo@gmail.com>
2024-08-30 19:08:35 -0700

    add code highlighter

--- src/lib/utils/markdown.js
+++ src/lib/utils/markdown.js
@@ -1,9 +1,31 @@
 import * as commonmark  from "commonmark";
+import { HighlightJS as hljs} from "highlight.js";
+import { parse as parseHTML } from "node-html-parser";
 
 const reader = new commonmark.Parser();
 const writer = new commonmark.HtmlRenderer();
 
 export const parse = (content) => {
 	const parsed = reader.parse(content);
-	return writer.render(parsed);
+	const output = writer.render(parsed);
+	return highlightCode(output);
+}
+
+function highlightCode(htmlStr) {
+	const root = parseHTML(htmlStr, { blockTextElements: { code: true }});
+	const elements = root.querySelectorAll("code");
+	elements.map(e => {
+		let lang = null;
+		let code = "";
+		if (e.attributes["class"]) {
+			lang = e.attributes["class"].split("-")[1];
+		}
+		if (lang) {
+			code = hljs.highlight(e.text, { language: lang}).value;
+		} else {
+			code = hljs.highlightAuto(e.text);
+		}
+		e.set_content(code);
+	});
+	return root.toString();
 }
