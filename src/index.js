111f2efeae1610d080802d197cd35ade22331c12
Ibrahim Diallo <ibuvdiallo@gmail.com>
2024-10-27 15:58:46 -0700

    upgrade toc.json

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ import { h, html } from "./lib/utils/html.js";
 import { renderPaths, render } from "./lib/utils/renderer.js";
 import path from "path";
 
-const toc = readJsonFile("./toc.json");
-const tocj = readJsonFile("./toc2.json");
+const tocj = readJsonFile("./toc.json");
 
 const BOOK_PATH = "book";
 const OUT_PATH = "website";
@@ -18,134 +17,130 @@ const notEmpty = (a) => !!a;
 const leadingZero = (num) => (num < 10 ? `0${num}`: `${num}`);
 
 const chaptFormat = (chapt) => {
+	if (chapt.root || chapt.section) {
+		return chapt.title;
+	}
 	if (!chapt.name) {
 		return chapt.title;
 	}
 	const num = +(chapt.name.split(" ")[1]);
-
 	return `CH${leadingZero(num)}: ${chapt.title}`;
 };
 
-const partFormat = (part) => {
-	if (!part.name) {
-		return part.title;
+const getToc = (id) => {
+	for(let i = 0, l = tocj.pages.length; i < l; i++) {
+		const page = tocj.pages[i];
+		if (page.id === id) {
+			return page;
+		}
 	}
-	return `${part.name}: ${part.title}`;
+	return false;
 };
 
-const dropExtension = (filename) => filename.replace(path.extname(filename), "");
+const getIntroUrl = () => getToc("toc").slug;
 
-const getIntroUrl = () => toc.sections.parts[0].slug;
+const getPLink = (id) => {
+	let index = 0;
+	for(let i = 0, l = tocj.toc.length; i <l; i++) {
+		if (tocj.toc[i] === id) {
+			index = i - 1;
+			break;
+		}
+	}
+	if (index < 0) {
+		return false;
+	}
+	const pageId = tocj.toc[index];
+	const current = getToc(pageId);
+	return html(h("a", { href: current.slug }, "&larr; " + chaptFormat(current)));
+};
 
-const buildSidebar = (selectedPart, selectedChapt) => {
+const getNLink = (id) => {
+	let index = 0;
+	for(let i = 0, l = tocj.toc.length; i <l; i++) {
+		if (tocj.toc[i] === id) {
+			index = i + 1;
+			break;
+		}
+	}
+	if (!tocj.toc[index]) {
+		return false;
+	}
+	const pageId = tocj.toc[index];
+	const current = getToc(pageId);
+	return html(h("a", { href: current.slug }, "&larr; " + chaptFormat(current)));
+};
+
+const buildSidebar = (pageId) => {
 	const filepath = path.join("src", "templates", "sidebar.template.html");
 	const content = readFileSync(filepath);
-	const parts = toc.sections.parts;
+	const parts = tocj.toc;
 	const obj = h("ul", { class: "sidebar"}, [
-		...parts.map(li => {
-			const chapts = li.chapters ?? [];
-			const partSelectedClass = li.slug === selectedPart.slug ? "selected" : "";
-			return h("li", { class: ["nav-part", partSelectedClass] }, [
-				h("h2", {}, li.slug ? h("a", { href: li.slug }, li.title) : li.title),
-				chapts.length ? h("ul", {}, [
-					...chapts.map(chapt => {
-						const chaptSelected = li.slug === selectedPart.slug && chapt.slug === selectedChapt?.slug ? "selected" : "";
-						return h("li", { class: ["nav-chapt", chaptSelected] }, 
-							h("a", { href: chapt.slug, title: chapt.title}, chaptFormat(chapt)),
-						);
-					})
-				]) : "",
-			]);
+		...parts.map(id => {
+			const current = getToc(id);
+			const isSelected = id === pageId
+			return h("li", { 
+					class: [
+						"nav-list__item", 
+						(current.root && "nav-list__root"), 
+						(current.section && "nav-list__part"),
+						(isSelected && "nav-list__selected")
+					].filter(notEmpty)
+				}, 
+				h("a", { href: current.slug }, 
+					current.root 
+						? current.title
+						: current.section
+						? current.name + ": " + current.title
+						: chaptFormat(current)
+				)
+			);
 		})
 	]);
 	const out = html(obj);
-	return render(filepath, { sidebar: out})
-};
+	return render(filepath, { sidebar: out});
+}
 
-const buildTOC2 = () => {
+const buildTOC = () => {
 	const pagesInOrder = tocj.toc;
 	const elements = [];
 
-	const get = (id) => {
-		for(let i = 0, l = tocj.pages.length; i < l; i++) {
-			const page = tocj.pages[i];
-			if (page.id === id) {
-				return page;
-			}
-		}
-		return false;
-	}
-
 	pagesInOrder.map(page => {
-		const current = get(page);
+		const current = getToc(page);
 		if(current.root) {
 			elements.push(
 				h("li", { class: "toc-list-root"}, 
-					h("a", { href: current.slug}, current.name)
+					h("a", { href: current.slug }, current.name)
 				)
-			)
+			);
 		} else if(current.section) {
 			elements.push(
 				h("li", { class: "toc-list-section"}, 
 					h("h2", {}, 
-						h("a", { href: current.slug}, [
+						h("a", { href: current.slug }, [
 							current.name,
 							": ",
 							current.title
 						])
 					)
 				)
-			)
+			);
 		} else {
 			elements.push(
 				h("li", { class: "toc-list-chapter"}, 
-					h("a", { href: current.slug}, [
-						h("span", { class: "toc-chapt__name"}, current.name+ ": "),
+					h("a", { href: current.slug }, [
+						h("span", { class: "toc-chapt__name"}, current.name + ": "),
 						current.title
 					])
 				)
-			)
+			);
 		}
 	});
 	
 	const output = html(h("ul", {}, elements));
-	const templatePath = path.join("src", "templates", "toc.template.html")
-	return render(templatePath, {
-		content: output,
-		TITLE: "Table of Content",
-		ASSET_PATH,
-	});
-};
+	const entry = getToc("toc");
 
-const buildTOC = () => {
-	const elements = [];
-	const roman = ["0", "0", "I", "II", "III", "IV", "V"];
-	toc.sections.parts.map( (part, index) => {
-		const chapts = (() => {
-			if(!part.chapters) return [];
-			return h("ul", {}, part.chapters.map((c) => {
-				return h("li", {}, h("a", { href: c.slug}, [
-					...(
-						c.name
-							? [h("span", { class: "toc-chapt__name"}, c.name), ": "]
-							: []
-					),
-					h("span", { class: "toc-chapt__title"}, c.title+" "),
-				]));
-			}))
-		})();
-		const rank = index < 2 ? part.title : `Part ${roman[index]}: ${part.title}`;
-		elements.push(
-			h("li", { class: "toc-list"}, 
-				h("div", { }, [
-					h("h2", {}, part.slug ? h("a", { href: part.slug }, rank) : rank),
-					chapts,
-				])
-			)
-		)
-	});
-	const output = html(h("ul", {}, elements));
-	const templatePath = path.join("src", "templates", "toc.template.html")
+	const templatePath = path.join("src", "templates", entry.template)
 	return render(templatePath, {
 		content: output,
 		TITLE: "Table of Content",
@@ -154,88 +149,15 @@ const buildTOC = () => {
 };
 
 function buildFront() {
-	const contentPath = path.join(BOOK_PATH, toc.front.content);
-	const frontTemplate = path.join("src", "templates", toc.front.template);
+	const front = getToc("homepage");
+	const contentPath = path.join(BOOK_PATH, front.content);
+	const frontTemplate = path.join("src", "templates", front.template);
+
 	const output = renderPaths(contentPath, frontTemplate, {
 		ASSET_PATH,
 		FRONT_URL: getIntroUrl(),
 	});
 	return output;
-}
-
-function getPreviousLink(partIndex, chaptIndex) {
-	// moveing from part to previous chapter
-	if (chaptIndex === null) {
-		const part = toc.sections.parts[partIndex - 1];
-		if (!part) {
-			return "";
-		}
-
-		if (part && !part.chapters) {
-			return html(h("a", { href: part.slug }, "&larr; " + partFormat(part)));
-		}
-		chaptIndex = part.chapters.length - 1;
-		const chapt = part.chapters[chaptIndex];
-		return html(h("a", { href: chapt.slug }, "&larr; " + chaptFormat(chapt)));
-	}
-
-	// moving from chapter to previous part
-	chaptIndex--;
-	if(chaptIndex < 0) {
-		let part = toc.sections.parts[partIndex];
-		// skip parts that don't have pages
-		if (!part.slug) {
-			part = toc.sections.parts[partIndex - 1];
-			if (part && part.chapters) {
-				let chapt = part.chapters[part.chapters.length - 1];
-				return html(h("a", { href: chapt.slug }, "&larr; " + chaptFormat(chapt)));
-			}
-			if (!part) {
-				return "";
-			}
-			return html(h("a", { href: part.slug }, "&larr; " + partFormat(part)));
-		}
-		return html(h("a", { href: part.slug }, "&larr; " + partFormat(part)));
-	}
-	const part = toc.sections.parts[partIndex]
-	if (!part.chapters) {
-		return html(h("a", { href: part.slug }, "&larr; " + partFormat(part)));
-	}
-	const chapt = part.chapters[chaptIndex];
-	return html(h("a", { href: chapt.slug }, "&larr; " + chaptFormat(chapt)));
-}
-
-function getNextLink(partIndex, chaptIndex) {
-	// moving from part to next chapter
-	if (chaptIndex === null) {
-		const part = toc.sections.parts[partIndex];
-		if (!part) {
-			return "";
-		}
-		if (!part.chapters) {
-			return "";
-			// return  html(h("a", { href: part.slug }, partFormat(part) + " &rarr;"));
-		}
-		const chapt = part.chapters[0];
-		return html(h("a", { href: chapt.slug }, chaptFormat(chapt) + " &rarr;"));
-	}
-
-	// moving from chapter to next part
-	chaptIndex++;
-	if (chaptIndex >= toc.sections.parts[partIndex].chapters.length) {
-		chaptIndex = 0;
-		partIndex++;
-		const part = toc.sections.parts[partIndex];
-		if (!part) {
-			return "";
-		}
-		return  html(h("a", { href: part.slug }, partFormat(part) + " &rarr;"));
-	}
-
-	// moving to the next chapter
-	const part = toc.sections.parts[partIndex];
-	const chapt = part.chapters[chaptIndex];
-	return html(h("a", { href: chapt.slug }, chaptFormat(chapt) + " &rarr;"));
 }
 
 const formatChapterHeader = (chapt, title) => {
@@ -263,54 +185,31 @@ const getMeta = (part) => {
 function buildParts() {
 	const obj = {};
 	obj["content.html"] = buildTOC();
-	obj["temp.html"] = buildTOC2();
 
-	toc.sections.parts.map( (part, partIndex) => {
-		if (part.name === "toc") {
+	tocj.toc.map(id => {
+		const current = getToc(id);
+		if (current.skip || !current.template || current.content === "auto") {
 			return ;
 		}
+		const sidebar = buildSidebar(id);
+		const templatePath = path.join("src", "templates", current.template);
+		const fullPath = [BOOK_PATH, ...current.content.split("/")].filter(notEmpty);
+		const contentPath = path.join(...fullPath);
+		const giturl = [GITROOT, current.content].join("/");
+		const metaTags = getMeta(current);
 
-		if (part.template) {
-			const sidebar = buildSidebar(part);
-			const fullPath = [BOOK_PATH, part.folder, part.content].filter(notEmpty);
-			const contentPath = path.join(...fullPath);
-			const templatePath = path.join("src", "templates", part.template);
-			const giturl = [GITROOT, part.folder, part.content].join("/");
-			const metaTags = getMeta(part);
-			const output = renderPaths(contentPath, templatePath, {
-				sidebar,
-				PAGE_TITLE: part.title,
-				META_TAGS: metaTags,
-				TITLE: html(h("h1", {}, part.title)),
-				ASSET_PATH,
-				PreviousURL: getPreviousLink(partIndex, null),
-				NextURL: getNextLink(partIndex, null),
-				GITURL: html(h("a", { href: giturl, target: "_blank" }, "Github Source")),
-			});
-			obj[part.slug] = output;
-		}
-		// chapter
-		if (!part.chapters) { return; }
-
-		part.chapters.map((chapt, chaptIndex) => {
-			const sidebar = buildSidebar(part, chapt);
-			const chapTemplate = path.join("src", "templates", chapt.template);
-			const contentPath = path.join(...["book", part.folder, chapt.content].filter(notEmpty));
-			const giturl = [GITROOT, part.folder, chapt.content].join("/");
-			const pageTitle = chapt.name ? `${chapt.name}: ${chapt.title}` : chapt.title;
-			const output = renderPaths(contentPath, chapTemplate, {
-				sidebar,
-				PAGE_TITLE: pageTitle,
-				META_TAGS: "",
-				TITLE: formatChapterHeader(chapt.name, chapt.title),
-				ASSET_PATH,
-				PreviousURL: getPreviousLink(partIndex, chaptIndex),
-				NextURL: getNextLink(partIndex, chaptIndex),
-				GITURL: html(h("a", { href: giturl, target: "_blank" }, "Github Source")),
-			});
-			obj[chapt.slug] = output;
+		const output = renderPaths(contentPath, templatePath, {
+			sidebar,
+			PAGE_TITLE: current.title,
+			META_TAGS: metaTags,
+			TITLE: html(h("h1", {}, current.title)),
+			ASSET_PATH,
+			PreviousURL: getPLink(id),
+			NextURL: getNLink(id),
+			GITURL: html(h("a", { href: giturl, target: "_blank" }, "Github Source")),
 		});
 
+		obj[current.slug] = output;
 	});
 	return obj;
 }
@@ -332,7 +231,7 @@ const SiteUrl = function(opt) {
 const buildSitemap = () => {
 	const urls = [
 		(new SiteUrl({
-			loc: `https://automatedagentsbook.com/index.html`,
+			loc: `https://automatedagentsbook.com/`,
 			priority: 1,
 		})).toString(),
 		(new SiteUrl({
@@ -340,27 +239,17 @@ const buildSitemap = () => {
 			priority: 1,
 		})).toString()
 	];
-	toc.sections.parts.map( (part, partIndex) => {
-		if (part.name === "toc") {
+	tocj.toc.map(id => {
+		const current = getToc(id);
+		if (current.skip || !current.template || current.content === "auto") {
 			return ;
 		}
-		if(!part.slug) {
-
-		} else {
-			urls.push((new SiteUrl({
-				loc: `https://automatedagentsbook.com/${part.slug}`,
-				priority: 1,
-			})).toString())
-		}
-		
-
-		part.chapters?.map((chapt, chaptIndex) => {
-			urls.push((new SiteUrl({
-				loc: `https://automatedagentsbook.com/${chapt.slug}`,
-			})).toString())
-		});
-
+		urls.push((new SiteUrl({
+			loc: `https://automatedagentsbook.com/${current.slug}`,
+			priority: current.section ? 1 : .75,
+		})).toString());
 	});
+	
 	let content = [
 		`<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">`,
 		...urls,
