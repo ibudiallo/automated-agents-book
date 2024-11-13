c05d8057037263aa91df8bef1386e7733c2e508a
Ibrahim Diallo <ibuvdiallo@gmail.com>
2024-11-12 18:02:31 -0800

    checkpoint

--- code/nodesys/js/nodesys.js
+++ code/nodesys/js/nodesys.js
@@ -1,9 +1,12 @@
 const SVG = (() => {
+	const SVG_NS = "http://www.w3.org/2000/svg";
 	const { h, render } = JML();
 	const DEFAULT_WIDTH = 200;
+	const PADDING = 6;
+	const LINE_HEIGHT = 12;
 
 	const svgWrapper = (content) => {
-		return h("svg", { version: "1.1"}, content);
+		return h("svg", { version: "1.1", width: 600, height: 600}, content);
 	}
 	const drawBox = (node) => {
 		const width = 300,
@@ -29,20 +32,102 @@ const SVG = (() => {
 		return render(root, html);
 	};
 
+	const makeDraggable = (node, title) => {
+		let topParent = node;
+		while(topParent) {
+			topParent = node.parentNode;
+			if (topParent.tagName === "svg") {
+				break;
+			}
+		}
+		node.addEventListener("click", (e) => {
+			const [tX, tY]= node.getAttribute("transform")
+				.replace(/(translate)|[\(\)\s]/g, "")
+				.split(",")
+				.map(a => parseFloat(a));
+			const x = e.clientX - tX;
+			const y = e.clientY - tY;
+
+			//console.log("aa", x, y)
+			//node.setAttribute("transform", `translate(${x}, ${x})`);
+		})
+		const getMousePosition = (e) => {
+			var CTM = topParent.getScreenCTM();
+			return {
+				x: Math.round((e.clientX - CTM.e) / CTM.a),
+				y: Math.round((e.clientY - CTM.f) / CTM.d),
+				offsetX: e.clientX - e.offsetX,
+				offsetY: e.clientY - e.offsetY
+			};
+		};
+		const startDrag = () => {
+			node.addEventListener("mousemove", drag, false);
+			window.addEventListener("mouseup", endDrag);
+		};
+		const drag = (e) => {
+			const pos = getMousePosition(e)
+			const [tX, tY]= node.getAttribute("transform")
+				.replace(/(translate)|[\(\)\s]/g, "")
+				.split(",")
+				.map(a => parseFloat(a));
+			const x = pos.x - tX;
+			const y = pos.y - tY;
+			console.log(x, y)
+			node.setAttribute("transform", `translate(${x}, ${y})`);
+		};
+		const endDrag = () => {
+			node.removeEventListener("mousemove", drag);
+			window.removeEventListener("mouseup", endDrag);
+		};
+		title.addEventListener("mousedown", startDrag);
+		title.addEventListener("mouseup", endDrag);
+/*
+		let isDown = false;
+		title.addEventListener("mousedown", () => {
+			if(isDown) {
+				return;
+			}
+			isDown = true;
+			const mouseUp = () => {
+				console.log("Up")
+				isDown = false;
+				window.removeEventListener("mouseup", mouseUp);
+				window.removeEventListener("mousemove", mousemove);
+			};
+			const mousemove = (e) => {
+				const x = e.clientX - coord.x - e.offsetX;
+				const y = e.clientY - coord.y - e.offsetY;
+				node.setAttribute("transform", `transform(${x}, ${y})`);
+			}
+			window.addEventListener("mouseup", mouseUp, false);
+			window.addEventListener("mousemove", mousemove, false)
+
+		}, false);
+		*/
+	};
+
 	const commentNode = (data) => {
+		const parentNode = {};
 		const width = 200 || DEFAULT_WIDTH;
-		return h("g", { width, id: `comment-${data.id}`, transform: `translate(${data.coord.x}, ${data.coord.y})`}, [
+		const onCreate = (e) => {
+			parentNode.node = e.target;
+		};
+		return h("g", { width, id: `comment-${data.id}`, transform: `translate(${data.coord.x}, ${data.coord.y})`, onCreate}, [
 			h("rect", { width, height: 100, rx : 6, x: 0, y: 0, fill: "#333"}),
-			titleBar(data.title, { width }),
+			titleBar(data.title, { width }, parentNode),
 			textSnippet(data.content),
 		]);
 	}
 
-	const titleBar = (title, props) => {
-		return h("g", { ...props }, [
-			h("rect", { width: "100%", height: 24, rx : 3, x: 0, y: 0, fill: "#cdcdcd", ...props}),
-			h("circle", { cx: 12, cy: 12, fill: "#8bc34a", r: 6}),
-			h("text", { x: 24, y: 17 }, title),
+	const titleBar = (title, props, parent) => {
+		const onCreate = (e) => {
+			parent.titleBar = e.target;
+			makeDraggable(parent.node, e.target);
+		};
+		return h("g", { ...props, onCreate }, [
+			h("rect", { width: "100%", height: LINE_HEIGHT * 2, rx : 3, x: 0, y: 0, fill: "#cdcdcd", ...props}),
+			h("circle", { cx: PADDING * 2, cy: PADDING * 2, fill: "#8bc34a", r: 6}),
+			h("text", { x: 24, y: 17, style: "pointer-events: none" }, title),
 		]);
 	};
 
@@ -89,7 +174,7 @@ const JsonData = {
 		id: "0000002",
 		type: "Comment",
 		title: "Comment",
-		content: "Here we make a long comment",
+		content: "Enter comment here",
 		parent: null,
 		coord: {
 			x: 0,
